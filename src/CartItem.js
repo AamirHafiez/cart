8efc28a4e6952aae4124416fe1dce54f4ae7dc2a
Aamir <aamirhafiez@gmail.com>
2020-10-01 23:06:44 +0530

    Increment and Decrement functionality added to the item

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class CartItem extends React.Component{
 
+    // * ------- default state using the constructor --------
     constructor() {
         super();
         this.state = {
@@ -10,23 +11,54 @@ class CartItem extends React.Component{
             qty: 1,
             img: ''
         }
-        // Method 1.1: either use bind to bind 'this' to a function and simply create a function
-        // example below 
+        // * Method 1.1: either use bind to bind 'this' to a function and simply create a function
+        // * example below 
         // this.increaseQuantity = this.increaseQuantity.bind(this);
 
-        // Method 1.2 or simply use arrow function to automatically bind 'this' to a function 
+        // *Method 1.2 or simply use arrow function to automatically bind 'this' to a function 
     }
 
-    // Method 1.1: when using normal function and binding this in constructor
+    // * ------- TO INCREASE QUANTITY OF THE ITEM ------------
+    // * Method 1.1: when using normal function and binding this in constructor
     // increaseQuantity()  {
     //     this.state.qty += 1;
     //     console.log(this.state.qty);
     // }
 
-    // Method 1.2: when using arrow function to bind 'this'
+    // * Method 1.2: when using arrow function to bind 'this'
     increaseQuantity = () => {
-        this.state.qty += 1;
-        console.log(this.state.qty);
+        // * below code changes the quantity but it is not going to re-render it on the webpage
+        // * hence we will use inbuilt functions provided by React.component parent class
+        // this.state.qty += 1;
+
+        // * Method - 1 -----------------> if we just have to change value
+        // * the below function will change the state value and then re render it on the page
+        // * this setState() method will do shallow merging, i.e, only that value will be changed that we want to change, example here qty
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // });
+
+
+        // * Method - 2 -----------------> if we require previous state
+        // * using a callback arrow function inside of setState() method
+        // * here in prevState the previous state will be saved  
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1
+            }
+        });
+
+        // console.log(this.state.qty);
+    }
+
+    // to decrese the quantity of an item
+    decreaseQuantity = () => {
+        if(this.state.qty <= 0){
+            return;
+        }
+        this.setState({
+            qty: this.state.qty - 1
+        });
     }
 
     render(){
@@ -34,7 +66,7 @@ class CartItem extends React.Component{
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image} />
+                    <img alt="" style={styles.image} />
                 </div>
                 <div className="right-block">
                     <div style={{fontSize: 'xx-large'}}>{title}</div>
@@ -44,7 +76,7 @@ class CartItem extends React.Component{
                         {/* Buttons */}
                         {/* Method 1.1: Here we have used function at 'onClick' but to use 'this' here we need to bind 'this' to the function first */}
                         <img title="Add more" src="https://www.flaticon.com/svg/static/icons/svg/1828/1828817.svg" alt="increase" className="action-icons" onClick={this.increaseQuantity} />
-                        <img title="Delete more" src="https://www.flaticon.com/svg/static/icons/svg/753/753340.svg" alt="decrease" className="action-icons" />
+                        <img title="Delete more" src="https://www.flaticon.com/svg/static/icons/svg/753/753340.svg" alt="decrease" className="action-icons" onClick={this.decreaseQuantity} />
                         <img title="Remove from cart" src="https://www.flaticon.com/svg/static/icons/svg/3159/3159673.svg" alt="delete" className="action-icons" />
                     </div>
                 </div>
@@ -53,6 +85,7 @@ class CartItem extends React.Component{
     }
 }
 
+// * this is to set the css styles after creating this object we will use it in our jsx in style attribute
 const styles = {
     image: {
         height: 130,
