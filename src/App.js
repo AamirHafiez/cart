6625a9ef4813494b45a55f47bbdf6174845f2660
Aamir <aamirhafiez@gmail.com>
2020-11-24 07:36:15 +0530

    Integrated Firebase firestore database | and added respective functionalities

--- src/App.js
+++ src/App.js
@@ -1,129 +1,220 @@
 import React from 'react';
 import Cart from './Cart'
 import Navbar from './Navbar';
+import firebase from 'firebase/app'
 
 class App extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-        product: [
-            {
-                id: 1,
-                title: 'Laptop',
-                price: 70000,
-                qty: 1,
-                img: 'https://www.flaticon.com/svg/static/icons/svg/742/742201.svg'
-            },
-            {
-                id: 2,
-                title: 'USB',
-                price: 500,
-                qty: 1,
-                img: 'https://www.flaticon.com/svg/static/icons/svg/3233/3233479.svg'
-            },
-            {
-                id: 3,
-                title: 'Mobile',
-                price: 23000,
-                qty: 1,
-                img: 'https://www.flaticon.com/svg/static/icons/svg/2097/2097276.svg'
-            }
-        ]
-    }
-}
-
-handleIncreaseQuantity = (x) => {
-    let p = this.state.product;
-    let index = p.indexOf(x);
-    p[index].qty += 1;
-
-    this.setState({
-        product: p
-    });
-}
-
-handleDecreaseQuantity = (x) => {
-    let p = this.state.product;
-    let index = p.indexOf(x);
-    
-    if(p[index].qty === 0){
-        return;
+    constructor(){
+        super();
+        this.state = {
+            product: [],
+            loading: true
+        }
+        // Instead of using firebase.firestore() everywhere we could just use this.db
+        // this.db = firebase.firestore();
     }
 
-    p[index].qty -= 1;
-    this.setState({
-        product: p
-    });
-}
+    componentDidMount() {
+        // Reading data from firestore
+        firebase // from firebase 
+            .firestore() // database called
+            .collection('products') // the collection from the database
+            // .orderBy('price') // to sort in increasing order
+            .where('price', '>=', 25000)
+            // we can use multiple where example
+            // .where()
+            // .where()
+            .orderBy('price', 'desc') // to sort in decreasing order
+            //.get() // gets the instance of the collection and returns a promise
+            //.then((snapshot) => { // promise resolves and the callback has the data from the database
+            .onSnapshot( (snapshot)=> { // instead of get and then we use onSnapshot which will listen to any changes in the database, i.e, we don't need to refresh browser if database changes
+                let product = snapshot.docs.map((i) => { // docs contains the array of data and map function returns the array
+                    let o = i.data(); // here i.data() function gives the actual object containing our fields
+                    o['id'] = i.id; // we create another field in our object to store id
+                    return o; // and return this object which will be stored as an element in our product array
+                });
 
-handleDeleteProduct = (x) => {
+                // then we set the state with all the objects from firestore
+                this.setState({
+                    product,
+                    loading: false
+                });
+            });
+    }
 
-    // Using Filter Function (if x is an id):-
-    // if we had fetched a parameter that is the object id instead of x here(i.e, the whole object)
-    // we could have used another function of javascript called as filter.
-    // filter works by filtering the array according to the need (condition given in the filter function)
+    addProduct = () => {
+        firebase
+            .firestore()
+            .collection('products')
+            .add({ // this is used to add a new product in the collection of the firebase
+                title: 'Television',
+                price: 35000,
+                qty: 1,
+                img: 'https://www.flaticon.com/svg/static/icons/svg/420/420424.svg'
+            })
+            .then((docRef) => { // add function returns promise which needs to be handled
+                console.log('Product added to document with reference:', docRef);
+            })
+            .catch((error) => {
+                console.log('Error:', error);
+            });
+    }
+
+    handleIncreaseQuantity = (x) => {
+        let p = this.state.product;
+        let index = p.indexOf(x);
+        // p[index].qty += 1;
+
+        // this.setState({
+        //     product: p
+        // });
+
+        // here we need to first get the reference of the document we need to update
+        // we use doc function to get the reference from the database
+        // we need to provide the id of the document in the doc method
+        const docRef = firebase.firestore().collection('products').doc(p[index].id);
+        docRef.update({ // using reference we can update
+            qty: p[index].qty + 1
+        })
+        .then(() => {
+            console.log('Updated Successfully -> increase qty');
+        })
+        .catch((error) => {
+            console.log('Error:', error);
+        });
+    }
+
+    handleDecreaseQuantity = (x) => {
+        let p = this.state.product;
+        let index = p.indexOf(x);
     
-    // example (if x would have been an id of an object to be deleted):-
-    // let { p } = this.state;
-    // let temp = p.filter( (i) => i.id != x ); ----> This would have given us a array in temp without id x
-    // this.setState({
-        // product: temp
-    //});
+        if(p[index].qty === 0){
+            return;
+        }
 
-    // Using splice function (if x is an object):-
-    let p = this.state.product;
-    let index = p.indexOf(x);
+        // p[index].qty -= 1;
+        // this.setState({
+        //     product: p
+        // });
 
-    p.splice(index, 1);
+        const docRef = firebase.firestore().collection('products').doc(p[index].id);
+        docRef.update({
+            qty: p[index].qty - 1
+        })
+        .then(() => {
+            console.log('Updated Successfully -> decrease qty');
+        })
+        .catch((error) => {
+            console.log('Error:', error);
+        });
+    }
 
-    this.setState({
-        product: p
-    });
-}
+    handleDeleteProduct = (x) => {
 
-getProductCount = () => {
-    let count = 0;
-    let {product} = this.state;
+        // Using Filter Function (if x is an id):-
+        // if we had fetched a parameter that is the object id instead of x here(i.e, the whole object)
+        // we could have used another function of javascript called as filter.
+        // filter works by filtering the array according to the need (condition given in the filter function)
+        
+        // example (if x would have been an id of an object to be deleted):-
+        // let { p } = this.state;
+        // let temp = p.filter( (i) => i.id != x ); ----> This would have given us a array in temp without id x
+        // this.setState({
+            // product: temp
+        //});
 
-    product.forEach((i) => {
-        count += i.qty
-    });
+        // Using splice function (if x is an object):-
+        let p = this.state.product;
+        let index = p.indexOf(x);
 
-    // or
-    // product.map((i) => {
-    //     count += i.qty;
-    // });    
-    return count;
-}
+        // p.splice(index, 1);
 
-getTotalAmount = () => {
-    let total = 0;
-    let { product } = this.state;
+        // this.setState({
+        //     product: p
+        // });
 
-    product.forEach((i) => {
-        total += i.qty * i.price
-    });
+        const docRef = firebase.firestore().collection('products').doc(p[index].id);
+        docRef.delete()
+        .then(() => {
+            console.log('Deleted Successfully');
+        })
+        .catch((error) => {
+            console.log('Error:', error);
+        });
+    }
 
-    return total;
-}
+    getProductCount = () => {
+        let count = 0;
+        let {product} = this.state;
+
+        product.forEach((i) => {
+            count += i.qty
+        });
+
+        // or
+        // product.map((i) => {
+        //     count += i.qty;
+        // });    
+        return count;
+    }
+
+    getTotalAmount = () => {
+        let total = 0;
+        let { product } = this.state;
+
+        product.forEach((i) => {
+            total += i.qty * i.price
+        });
+
+        return total;
+    }
 
   render() {
+
+    const { loading } = this.state;
+
     return (
       <div className="App">
         <Navbar 
             count= {this.getProductCount()}
             total = {this.getTotalAmount()} 
         />
+
+        {
+            loading 
+        
+            && 
+        
+            <div style={styles.loading}>
+                <img style={styles.loadingGif} src="https://i.imgur.com/GLdqYB2.gif" alt="Loading..."/>
+            </div>
+        }
+
+        
         <Cart
           handleIncreaseQuantity = {this.handleIncreaseQuantity}
           handleDecreaseQuantity = {this.handleDecreaseQuantity}
           handleDeleteProduct = {this.handleDeleteProduct}
           product = {this.state.product}
         />
+
+        <button onClick={this.addProduct}>Add an Item</button>
+
       </div>
     );  
   }
+}
+
+var styles = {
+    loadingGif: {
+        height: 50,
+        width: 50
+    },
+    loading: {
+        textAlign: "center",
+        marginTop: 50
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+// generate this api from firebase.google.com
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
